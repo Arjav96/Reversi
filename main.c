cf995732f668bc3b360703135fdd6c5257ac3f6d
e320137 <e320137@localhost>
2014-11-14 10:14:41 +0100

    Inicializa e imprime

--- main.c
+++ main.c
@@ -1,18 +1,44 @@
-/* 
- * File:   main.c
- * Author: e320137
- *
- * Created on 14 de noviembre de 2014, 9:55
- */
-
 #include <stdio.h>
-#include <stdlib.h>
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+typedef enum {
+    FICHA_VACIO = 0,
+    FICHA_NEGRA,
+    FICHA_BLANCA
+} Ficha;
 
-    return (EXIT_SUCCESS);
+Ficha tablero[8][8];
+
+const char simbolo[3] = {'.', 'X', 'O'};
+
+void inicializar();
+void imprimir();
+
+int main() {
+    printf("Reversi\n");
+    inicializar();
+    imprimir();
 }
 
+void inicializar() {
+    int x, y;
+    
+    for(x = 0; x < 8; ++x) {
+        for(y = 0; y < 8; ++y) {
+            tablero[x][y] = FICHA_VACIO;
+        }
+    }
+    
+    tablero[3][3] = tablero[4][4] = FICHA_BLANCA;
+    tablero[3][4] = tablero[4][3] = FICHA_NEGRA;
+}
+
+void imprimir() {
+    int x, y;
+    
+    for(x = 0; x < 8; ++x) {
+        for(y = 0; y < 8; ++y) {
+            printf("%c", simbolo[tablero[x][y]]);
+        }
+        printf("\n");
+    }
+}
