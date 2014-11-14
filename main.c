22b578fe79481a6ed39a2259d4cc5413ee59fb1e
e320137 <e320137@localhost>
2014-11-14 10:54:45 +0100

    comprobar_movimiento

--- main.c
+++ main.c
@@ -10,6 +10,8 @@ const char simbolo[3] = {'.', 'X', 'O'};
 
 void inicializar(Ficha tablero[8][8]);
 void imprimir(Ficha tablero[8][8]);
+int dentro(int x, int y);
+int comprobar_movimiento(Ficha tablero[8][8], Ficha jugador, int f, int c);
 
 int main() {
     Ficha tablero[8][8];
@@ -41,4 +43,65 @@ void imprimir(Ficha tablero[8][8]) {
         }
         printf("\n");
     }
+}
+
+int dentro(int x, int y) {
+    return x >= 0 && x < 8 && y >= 0 && y < 8;
+}
+
+int comprobar_movimiento(Ficha tablero[8][8], Ficha jugador, int f, int c) {
+    const int dir[8][2] = {
+        {-1, 0},    //N
+        {-1, 1},    //NE
+        {0, 1},     //E
+        {1, 1},     //SE
+        {1, 0},     //S
+        {1, -1},    //SW
+        {-1, 0},    //W
+        {-1, 1}     //NW
+    };
+    
+    int d, x, y;
+    int encontrado_enemigo;
+    int posible;
+    
+    for(d = 0; d < 8; ++d) {
+        encontrado_enemigo = 0;
+        posible = 0;
+        x = f;
+        y = c;
+        
+        for(;;) { //bucle-avanzar
+            x += dir[d][0];
+            y += dir[d][1];
+            
+            if(dentro(x, y)) {
+                if(encontrado_enemigo) {
+                    if(tablero[x][y] == FICHA_VACIO) {
+                        posible = 0;
+                        break;
+                    } else if(tablero[x][y] == jugador) {
+                        posible = 1;
+                        break;
+                    }
+                } else {
+                    if(tablero[x][y] == jugador || tablero[x][y] == FICHA_VACIO) {
+                        posible = 0;
+                        break;
+                    } else { //Ficha enemiga
+                        encontrado_enemigo = 1;
+                    }
+                }
+            } else {
+                posible = 0;
+                break;
+            }
+        } //end bucle-avanzar
+        
+        if(posible) {
+            return 1;
+        }
+    }
+    
+    return 0;
 }
