40800473a92c5cd405e3f2c4f439b287c833d129
e320137 <e320137@localhost>
2014-11-14 09:58:07 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: e320137
+ *
+ * Created on 14 de noviembre de 2014, 9:55
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
